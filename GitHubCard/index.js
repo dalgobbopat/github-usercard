f42a660ae198a4fceb4580fe6e44a73798d901e8
Patrick Dal Gobbo <juandalgobbo@Juans-MacBook-Air.local>
2020-03-12 17:05:21 -0400

    made myCard obj

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,13 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/dalgobbopat')
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.log('the data was not retured', error)
+    });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -45,6 +52,20 @@ const followersArray = [];
 </div>
 
 */
+function MyCard(obj) {
+    const newCard = document.createElement('div'),
+        image = document.createElement('img'),
+        name = document.createElement('h3'),
+        userName = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        gitAddress = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
+
+
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
